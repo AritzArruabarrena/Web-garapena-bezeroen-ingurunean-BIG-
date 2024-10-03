364ee38cdbc0adb33dfc6f3c77dd7c2505d29c74
AritzArruabarrena <arruabarrena.aritz@uni.eus>
2024-10-03 12:41:43 +0200

    es6

---
+++ ES6/js/es6.js
@@ -0,0 +1,37 @@
+class Estudiante {
+  constructor(nombre, edad, ...asignaturas){
+      this.nombre = nombre;
+      this.edad = edad;
+      this.asignaturas = asignaturas;
+  }
+}
+
+Estudiante.prototype.saludar = () => console.log(`Hola, me llamo ${nombre} y tengo ${edad} años.`);
+
+Estudiante.prototype.listarAsignaturas = () => console.log(`Mis asignaturas son: ${this.asignaturas.join(', ')}`);
+
+let estudiante1 = new Estudiante('Ana', 20, 'Matemáticas', 'Historia', 'Literatura');
+  
+
+const {nombre, edad, asignaturas} = estudiante1;
+  
+
+console.log(`Nombre: ${nombre}`);
+console.log(`Edad: ${edad}`);
+  
+
+estudiante1.saludar();
+estudiante1.listarAsignaturas(asignaturas);
+  
+
+function calcularPromedio() {
+  let suma = 0;
+  for (let i = 0; i < arguments.length; i++) {
+      suma += arguments[i];
+  }
+  return suma / arguments.length;
+}
+
+
+let promedio = calcularPromedio(85, 90, 78, 92);
+console.log(`El promedio es: ${promedio}`);
